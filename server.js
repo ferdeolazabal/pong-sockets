397164a9084f92814d4f5846e296679c5e22bbef
ferdeolazabal <ferdeolazabal@gmail.com>
2024-08-05 22:11:42 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+const server = require("http").createServer();
+
+const PORT = 3000;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, () => console.log(`On http://localhost:${PORT}`));
+
+let readyPlayers = 0;
+
+io.on("connection", (socket) => {
+  console.log("New client connected =>", socket.id);
+
+  socket.on("ready", () => {
+    console.log("Player ready =>", socket.id);
+    readyPlayers++;
+
+    if (readyPlayers === 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
+});
