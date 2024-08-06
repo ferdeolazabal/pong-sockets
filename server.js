1be7b95779689c4cba42575735cbedea7020be0e
ferdeolazabal <ferdeolazabal@gmail.com>
2024-08-06 00:01:53 -0300

    Apply express server for sockets & api server

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
 // @ts-nocheck
-const server = require("http").createServer();
+const http = require("http");
+const io = require("socket.io");
+
+const apiServer = require("./api");
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+const sockets = require("./sockets");
 
 const PORT = 3000;
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+httpServer.listen(PORT);
 
-server.listen(PORT, () => console.log(`On http://localhost:${PORT}`));
+console.log(`Listening on http://localhost:${PORT}`);
 
-let readyPlayers = 0;
-
-io.on("connection", (socket) => {
-  console.log("New client connected =>", socket.id);
-
-  socket.on("ready", () => {
-    console.log("Player ready =>", socket.id);
-    readyPlayers++;
-
-    if (readyPlayers % 2 === 0) {
-      io.emit("startGame", socket.id);
-    }
-  });
-
-  socket.on("paddleMove", (data) => {
-    socket.broadcast.emit("paddleMove", data);
-  });
-
-  socket.on("ballMove", (data) => {
-    socket.broadcast.emit("ballMove", data);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(`Client disconnected: ${socket.id} because ${reason}`);
-  });
-});
+sockets.listen(socketServer);
